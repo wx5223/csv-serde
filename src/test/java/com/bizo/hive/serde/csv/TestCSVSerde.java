056324dc1308479e6bbf79631127404991dff296
woodrad <mwoodyard+github@5lbs.org>
2015-05-16 15:06:50 -0500

    Name test class to fit Hadoop style. Use csv files for tests.

--- src/test/java/com/bizo/hive/serde/csv/TestCSVSerde.java
+++ src/test/java/com/bizo/hive/serde/csv/TestCSVSerde.java
@@ -1,21 +1,26 @@
 package com.bizo.hive.serde.csv;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import org.apache.hadoop.hive.serde.serdeConstants;
 import org.apache.hadoop.io.Text;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
-
-public final class CSVSerdeTest {
+public final class TestCSVSerde {
   private final CSVSerde csv = new CSVSerde();
   final Properties props = new Properties();
+  private static final Logger LOGGER = Logger.getLogger(
+      TestCSVSerde.class.getName());
 
   @Before
-  public void setup() throws Exception {
+  public void setup() {
     props.put(serdeConstants.LIST_COLUMNS, "a,b,c,d");
     props.put(serdeConstants.LIST_COLUMN_TYPES, "string,string,string,string");
   }
@@ -24,7 +29,10 @@ public final class CSVSerdeTest {
   public void testDeserialize() throws Exception {
     csv.initialize(null, props);
 
-    final Text in = new Text("hello,\"yes, okay\",1,\"new\nline\"");
+    String csvFile = new String(Files.readAllBytes(Paths
+        .get("./src/test/resources/testDeserialize.csv")));
+
+    final Text in = new Text(csvFile);
     final List<String> row = (List<String>) csv.deserialize(in);
 
     assertEquals("hello", row.get(0));
@@ -34,13 +42,32 @@ public final class CSVSerdeTest {
   }
 
   @Test
+  public void testDeserializeSingle() throws Exception {
+    csv.initialize(null, props);
+
+    String csvFile = new String(Files.readAllBytes(Paths
+        .get("./src/test/resources/testDeserializeSingle.csv")));
+
+    final Text in = new Text(csvFile);
+    final List<String> row = (List<String>) csv.deserialize(in);
+
+    assertEquals("hello", row.get(0));
+    assertEquals("yes, okay", row.get(1));
+    assertEquals("1", row.get(2));
+    assertEquals("no new line", row.get(3));
+  }
+
+  @Test
   public void testDeserializeCustomSeparator() throws Exception {
     props.put("separatorChar", "\t");
     props.put("quoteChar", "'");
 
     csv.initialize(null, props);
 
-    final Text in = new Text("hello\t'yes\tokay'\t1\t'new\n\nline'");
+    String csvFile = new String(Files.readAllBytes(Paths
+        .get("./src/test/resources/testDeserializeCustomSeparator.csv")));
+
+    final Text in = new Text(csvFile);
     final List<String> row = (List<String>) csv.deserialize(in);
 
     assertEquals("hello", row.get(0));
@@ -56,7 +83,10 @@ public final class CSVSerdeTest {
 
     csv.initialize(null, props);
 
-    final Text in = new Text("hello,'yes\\'okay',1,'new\r\nline'");
+    String csvFile = new String(Files.readAllBytes(Paths
+        .get("./src/test/resources/testDeserializeCustomEscape.csv")));
+
+    final Text in = new Text(csvFile);
     final List<String> row = (List<String>) csv.deserialize(in);
 
     assertEquals("hello", row.get(0));
@@ -73,7 +103,11 @@ public final class CSVSerdeTest {
 
     csv.initialize(null, props);
 
-    final Text in = new Text("hello,\"yes, okay\",1,\"new\r\n\r\n\"\"line\"\"\"");
+    String csvFile = new String(Files.readAllBytes(Paths
+        .get("./src/test/resources/"
+            + "testDeserializeCustomSeparatorCustomEscape.csv")));
+
+    final Text in = new Text(csvFile);
     final List<String> row = (List<String>) csv.deserialize(in);
 
     assertEquals("hello", row.get(0));
